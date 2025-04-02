17a7c0f5dae9e023bcf9ab6302444a01ad774c3f
yk893 <ykars893@gmail.com>
2025-04-02 23:04:05 +0300

    generations completed

--- js-08-New generation_operators/new_generation.js
+++ js-08-New generation_operators/new_generation.js
@@ -1,15 +1,15 @@
 console.log("**** NEW GEN OPERATORS *****");
 
-// * =============================================
-// *  DESTRUCTURING (OBJECT)
-// * =============================================
+//* =============================================
+//*  DESTRUCTURING (OBJECT)
+//* =============================================
 
-const product1={
-    id:"12345",
-    brand:"Apple",
-    type:"smart phone",
-    price:30000
-}
+const product1 = {
+  id: "12345",
+  brand: "Apple",
+  type: "smart phone",
+  price: 30000,
+};
 
 const product2 = {
   id: "12346",
@@ -18,225 +18,185 @@ const product2 = {
   price: 50000,
 };
 
-//? . notaion ile erisim
-
+//? . notation ile erişim
 console.log(product2.brand);
 console.log(product1.type);
 
-
-let yeni1 = product1.price
-
+let yeni1 = product1.price;
 console.log(yeni1);
-yeni1+=10
-
+yeni1 += 10;
 console.log(yeni1);
-
 console.log(product1.price);
 
-//? square bracket ile erisim
+//?square bracket
+console.log(product2["type"]);
 
-console.log(product2['type']);
+//?DESTRUCTURING yöntemi
 
-//?  DESTRUCTURING YONTEMI
-
-let {id,brand,price,type} = product1
+let { id, brand, price, type } = product1;
 
 console.log("ID", id);
 console.log("BRAND", brand);
 console.log("PRICE", price);
 console.log("TYPE", type);
 
-
-price += 34
+price += 34;
 console.log(price);
-console.log(product1.price);
+// console.log(product1.price);
 
-let {id:id2,brand:brand2,price:price2,type:type2} = product2
+let { id: id2, brand: brand2, price: price2, type: type2 } = product2;
 
 console.log(brand2);
 
-
-// //? NESTED DESTR
+//? NESTED DESTR
 const insanlar = {
-    kisi1: {
-      kimlikNo: "1234567890",
-      adi: "Ahmet",
-      soyadi: "Can",
-      meslek: "Mimar",
-      maas: 30000,
-    },
-    kisi2: {
-      kimlikNo: "44234567890",
-      adi: "Canan",
-      soyadi: "Can",
-      meslek: "Sosyal Gelişim Uzmani",
-      maas: 25000,
-    },
-  };
+  kisi1: {
+    kimlikNo: "1234567890",
+    adi: "Ahmet",
+    soyadi: "Can",
+    meslek: "Mimar",
+    maas: 30000,
+  },
+  kisi2: {
+    kimlikNo: "44234567890",
+    adi: "Canan",
+    soyadi: "Can",
+    meslek: "Sosyal Gelişim Uzmanı",
+    maas: 25000,
+  },
+};
 
-//    /* ---------------------------------- */
-//    /*              // 1. YOL             */
-//    /* ---------------------------------- */
+//*1.yol dest
+//? 1 level dest
 
-  //! 1 level destructing
+// const{kisi1,kisi2}=insanlar
 
-//   const {kisi1,kisi2} = insanlar
+// console.log(kisi1.adi);
 
-//   console.log(kisi1.adi);
-
-
-//! 2 level destructing
-
-// const {adi,meslek} = kisi1
-
+//? 2 level dest
+// const{adi,meslek}=kisi1
 // console.log(adi);
-
 // console.log(meslek);
-
 // console.log(kisi1.soyadi);
 
-
-// let {maas, adi:adi2} = kisi2
+// let {maas, adi:adi2}=kisi2
 
 // console.log(maas);
-
 // console.log(adi2);
 
+//*2.yol dest (iki seviyeli)
 
-   /* ---------------------------------- */
-//    /*              // 2. YOL             */  (iki seviyeli)
-   /* ---------------------------------- */
-
-const {kisi1:{adi,soyadi,meslek},kisi2:{adi: adi2, meslek :meslek2}} = insanlar
+const {
+  kisi1: { adi, soyadi, meslek },
+  kisi2: { adi: adi2, meslek: meslek2 },
+} = insanlar;
 
 console.log(meslek);
-
-  
-
+console.log(adi2);
 
 //* Example
 const team = [
-    {
-      name: "Josh",
-      surname: "Barry",
-      job: "developer",
-      age: 30,
-    },
-    {
-      name: "John",
-      surname: "Barry",
-      job: "tester",
-      age: 45,
-    },
-    {
-      name: "Hazel",
-      surname: "Nut",
-      job: "team lead",
-      age: 40,
-    },
-  ];
+  {
+    name: "Josh",
+    surname: "Barry",
+    job: "developer",
+    age: 30,
+  },
+  {
+    name: "John",
+    surname: "Barry",
+    job: "tester",
+    age: 45,
+  },
+  {
+    name: "Hazel",
+    surname: "Nut",
+    job: "team lead",
+    age: 40,
+  },
+];
 
+console.log("************************");
+team.forEach((kisi) => {
+  console.log(kisi.name);
+  console.log(kisi.surname);
+  console.log(kisi.job);
+  console.log(kisi.age);
+});
 
-  console.log("*********************");
+console.log("************************");
 
-  team.forEach((kisi)=>{
-    console.log(kisi.name);
-    console.log(kisi.surname);
-    console.log(kisi.job);
-    console.log(kisi.age);
-    
-  })
-  
-  console.log("*********************");
+team.forEach((kisi) => {
+  const { name, age, surname, job } = kisi;
 
-  team.forEach((kisi)=>{
-
-const {name,age,surname,job} = kisi
-
-    console.log(name);
-    console.log(surname);
-    console.log(job);
-    console.log(age);
-
-  })
-
-  console.log("*********************");
-
-  team.forEach((name,age,surname,job)=>{
-
-
-    console.log(name);
-    console.log(surname);
-    console.log(job);
-    console.log(age);
-
-  })
-
-  const objeGoster = function (){
-    return {
-      name: "Hazel",
-      surname: "Nut",
-      job: "team lead",
-      age: 40,
-    }
-  }
-
-  console.log(objeGoster());
-
-  let{name,surname,job} = objeGoster()
-
+  console.log(name);
+  console.log(surname);
   console.log(job);
+  console.log(age);
+});
 
-  //? parametreli fonksiyon
+console.log("************************");
+team.forEach(({ name, age, surname, job }) => {
+  console.log(name);
+  console.log(surname);
+  console.log(job);
+  console.log(age);
+});
 
-  console.log("*************");
+//!----  FUNCTIONLARDA DESTRUC. KULLANIMI ----
 
-  const printVeri = (veri)=>{
+const objeGoster = function () {
+  return {
+    name: "Hazel",
+    surname: "Nut",
+    job: "team lead",
+    age: 40,
+  };
+};
 
-console.log(veri.id, veri.bilgi, veri.tarih);
+console.log(objeGoster());
 
-  }
+let { name, surname, job } = objeGoster();
 
+console.log(job);
 
+//?parametreli fonksiyon
 
-  const printVeriDest = ( veri) =>{
-const {id, bilgi, tarih} = veri
+console.log("************************");
 
-console.log(id, bilgi, tarih);
+const printVeri = (veri) => {
+  console.log(veri.id, veri.bilgi, veri.tarih);
+};
 
+const printVeriDest = (veri) => {
+  const { id, bilgi, tarih } = veri;
 
-  }
+  console.log(id, bilgi, tarih);
+};
 
+const printVeriHavadaDest = ({ id, bilgi, tarih }) => {
+  console.log(id, bilgi, tarih);
+};
 
-  const printVeriHAvadaDest = ({id, bilgi, tarih}) =>{
-console.log(id, bilgi, tarih);
+const data = {
+  id: "123456",
+  bilgi: "teslim edildi",
+  tarih: "2 nisan",
+};
 
+printVeri(data);
+printVeriDest(data);
+printVeriHavadaDest(data);
 
-  }
-
-  const data = {
-    id:"12456",
-    bilgi:"teslim edildi",
-    tarih :"2 nisan"
-  }
-
-  printVeri(data)
-  printVeriDest(data)
-  printVeriHAvadaDest(data)
-  
 //* =============================================
 //*  DESTRUCTURING (ARRAY)
 //* =============================================
 
-const people = ["Muzaffar", "Mehmet", "Raife", "Mustafa", "Ozgur"]
+const people = ["Muzaffar", "Mehmet", "Raife", "Mustafa", "Özgür"];
 
+const [kisi1, kisi2, , kisi3] = people;
+console.log(kisi3);
 
- const [kisi1,kisi2,,kisi3] = people 
-
- console.log(kisi3);
-
-
- 
 //* ==============================================
 //*  REST (...)
 //* =============================================
@@ -245,51 +205,130 @@ const people = ["Muzaffar", "Mehmet", "Raife", "Mustafa", "Ozgur"]
 
 //! 1- Bir dizi veya object'deki bazi degerlerden geri kalanlarini ayri dizi yada objelere kopyalanmasini saglayabilir.
 
-//! ARRAY
-const takimlar = ["GS", "FB", "BJK", "TS", "DENIZLISPOR", "BURSA", "ISTANBUL"]
+//?ARRAY
+const takimlar = ["GS", "FB", "BJK", "TS", "DENİZLİSPOR", "BURSA", "İSTANBUL"];
 
-const [takim1,takim2,takim3, ...anadoluTakimlari] = takimlar
+const [takim1, takim2, takim3, ...anadoluTakimlari] = takimlar;
 
 console.log(anadoluTakimlari);
-console.log(takim2);
+console.log(takim1);
 
-//! OBJECTS
+//?OBJECT
 
-const person  ={
+const person = {
+  ad: "ipek",
+  soyad: "bilir",
+  meslek: "ev hanimi",
+  yas: 34,
+};
 
-  adi:"ipek",
-  soyadi:"bilir",
-  meslek:"ev hanimi",
-  yas:34
-}
-
-const {ad,soyad, ...geriKalan} = person
+const { ad, soyad, ...geriKalan } = person;
 
 console.log(geriKalan);
+console.log(geriKalan.yas);
 
-//! 2 - Bir fonksiyon argumanlarini diziye cevirmek icin kullanilir
+//! 2- Bir fonksiyon argümanlarını diziye çevirmek için kullanılır
 
-const topla1 = (...a)=>  {
+const topla1 = (...a) => {
   console.log(a);
 
-  return a.reduce((x,y)=> x+y)
-
-
-}
-
-const topla2 = (a,b,c, ...diger)=>{
-
+  return a.reduce((x, y) => x + y);
+};
+const topla2 = (a, b, c, ...diger) => {
   console.log(diger);
-  return a+b+c
-  
-}
+  return a + b + c;
+};
 
-console.log(topla1(5,6,7,8,9,22,454));
-console.log(topla2(1,3,5,60,0,2));
+console.log(topla1(5, 6, 7, 1, 9, 45, 8));
 
+console.log(topla2(1, 3, 5, 4, 8, 2));
 //? REST (...) ile non-iterable olan sayilari iterable hale (diziye) cevirmiş olduk.
 
-/* ---------------------------------- */
-/*               SPREAD               */
-/* ---------------------------------- */
+//* =============================================
+//*  SPREAD (...)
+//* =============================================
+
+//? Spread operatoru ise iterables olan bir elemani bireysel
+//? degerler haline getirir.
+
+const ucanAraclar = ["helikopter", "drone", "ucak", "fuze"];
+const karaAracları = ["araba", "bisiklet", "scooter", "motosiklet"];
+
+const tasitlar = [ucanAraclar, karaAracları];
+console.log(tasitlar);
+
+const tasitlar2 = [...ucanAraclar, ...karaAracları];
+console.log(tasitlar2);
+
+//ornek
+
+const cumle = "sen kalem ol bende kağıt";
+
+console.log([...cumle]);
+
+
+
+
+//? nested
+const sahislar = {
+  sahis1: {
+    name: "Can",
+    surname: "Canan",
+    dob: "1990",
+    job: "developer",
+    salary: "140000",
+    drivingLicense: true,
+  },
+  sahis2: {
+    name: "John",
+    surname: "Sweet",
+    dob: "1990",
+    job: "tester",
+    salary: "110000",
+    drivingLicense: false,
+  },
+  sahis3: {
+    name: "Steve",
+    surname: "Job",
+    dob: "2000",
+    job: "developer",
+    salary: "90000",
+    drivingLicense: true,
+  },
+};
+
+//? Javascript'de Objeler default olarak iterable degildir.
+//? Ama for in ve for of donguleri ile itere edilebilirler.
+
+//? Objelerin key ve value'larini okumak icin built-in metotlar vardir.
+//? Bu mettotlar aslinda objelerin key ve/veya value'lari bir dizi olarak dondurur.
+
+//!FOR IN
+
+for (const k in sahislar) {
+  console.log(k); //sahis1,sahis2,sahis3
+  console.log(sahislar[k].salary);
+}
+
+//?KULLANIŞLI OBJECT METODLARI
+
+console.log(Object.keys(sahislar)); //objenin keyleri array olarak döner
+
+console.log(Object.values(sahislar)); //objelerin value larını array olarak döndürür
+console.log(Object.values(sahislar)[1]);
+
+//!FOR OF
+
+for (const k of Object.values(sahislar)) {
+  console.log(k);
+
+  console.log(k.name);
+}
+
+for (const k of Object.keys(sahislar)) {
+  console.log(k);
+
+  if (k == "sahis2") console.log(sahislar[k].salary);
+}
+
 
